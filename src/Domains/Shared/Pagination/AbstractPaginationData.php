15bf7670cd12afe4e8177f3abc89daf07f211de7
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-07-28 17:22:47 -0400

    feat(pagination): create values initiais for pagination

--- src/Domains/Shared/Pagination/AbstractPaginationData.php
+++ src/Domains/Shared/Pagination/AbstractPaginationData.php
@@ -7,7 +7,7 @@ use Infrastructure\Shared\DataTransferObject;
 abstract class AbstractPaginationData extends DataTransferObject
 {
     public ?string $order;
-    public ?string $sort;
-    public ?string $per_page;
-    public ?string $page;
+    public string $sort = 'desc';
+    public int $per_page = 20;
+    public int $page = 1;
 }
