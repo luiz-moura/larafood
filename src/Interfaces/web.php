67947b2e1407a5f27c645fa5a16413da6c27f5d4
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-07-28 21:56:49 -0400

    feat(plans): create plan routes

--- src/Interfaces/web.php
+++ src/Interfaces/web.php
@@ -4,3 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Interfaces\Http\Plans\Controllers\PlanController;
 
 Route::get('admin/plans', [PlanController::class, 'index']);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [PlanController::class, 'index'])->name('admin.index');
+
+    Route::prefix('plan')->controller(PlanController::class)->group(function () {
+        Route::get('/', 'index')->name('plans.index');
+        Route::post('/', 'store')->name('plans.store');
+        Route::get('/create', 'create')->name('plans.create');
+        Route::get('/search', 'search')->name('plans.search');
+        Route::get('/{url}', 'show')->name('plans.show');
+        Route::delete('/{url}', 'destroy')->name('plans.destroy');
+        Route::get('/{url}/edit', 'edit')->name('plans.edit');
+        Route::put('/{url}', 'update')->name('plans.update');
+    });
+});
