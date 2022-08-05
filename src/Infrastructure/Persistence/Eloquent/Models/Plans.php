0e17d5188cf04e72edf633c795ebf68ad912dd31
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-08-05 14:54:00 -0400

    feat(plans): add detail relationship in plans model

--- src/Infrastructure/Persistence/Eloquent/Models/Plans.php
+++ src/Infrastructure/Persistence/Eloquent/Models/Plans.php
@@ -4,6 +4,7 @@ namespace Infrastructure\Persistence\Eloquent\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plans extends Model
 {
@@ -11,4 +12,9 @@ class Plans extends Model
 
     protected $table = 'plans';
     protected $fillable = ['name', 'url', 'price', 'description'];
+
+    public function details(): HasMany
+    {
+        return $this->hasMany('planDetails', 'plan_id', 'id');
+    }
 }
