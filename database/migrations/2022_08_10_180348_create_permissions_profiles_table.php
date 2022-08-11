681bc688c73166677c231d26a47a2c7a06644cab
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-08-10 20:34:55 -0400

    feat(ProfilesPermissions): create permissions_profiles migration

---
+++ database/migrations/2022_08_10_180348_create_permissions_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('permission_id')
+                ->constrained('permissions')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('profile_id')
+                ->constrained('profiles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions_profiles');
+    }
+};
