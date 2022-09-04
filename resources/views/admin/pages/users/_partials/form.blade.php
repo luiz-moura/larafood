cc16b8b955ebcb41a31d0b6f5dfea696e2c23dca
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-09-04 19:12:14 -0300

    feat(users): create users views

---
+++ resources/views/admin/pages/users/_partials/form.blade.php
@@ -0,0 +1,39 @@
+<x-alert-errors :errors="$errors"/>
+
+<div class="form-group">
+    <label for="name">Nome:</label>
+    <input type="text"
+           name="name"
+           id="name"
+           class="form-control"
+           placeholder="Nome:"
+           @isset($user) value="{{ $user->name }}" @endisset>
+</div>
+<div class="form-group">
+    <label for="email">E-mail:</label>
+    <input type="email"
+           name="email"
+           id="email"
+           class="form-control"
+           placeholder="E-mail:"
+           @isset($user) value="{{ $user->email }}" @endisset>
+</div>
+<div class="form-group">
+    <label for="password">Senha:</label>
+    <input type="password"
+           name="password"
+           id="password"
+           class="form-control"
+           placeholder="Senha:">
+</div>
+<div class="form-group">
+    <label for="password_confirmation">Confirmar senha:</label>
+    <input type="password"
+           name="password_confirmation"
+           id="password_confirmation"
+           class="form-control"
+           placeholder="Senha:">
+</div>
+<div class="form-group">
+    <button type="submit" class="btn btn-dark">Enviar</button>
+</div>
---
+++ resources/views/admin/pages/users/index.blade.php
@@ -0,0 +1,55 @@
+@extends('adminlte::page')
+
+@section('title', 'Usuários')
+
+@section('content_header')
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
+        <li class="breadcrumb-item active">Usuários</li>
+    </ol>
+    <h1>Usuários <a href="{{ route('users.create') }}" class="btn btn-dark"><i class="fas fa-plus-square"></i> Add</a></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <form action="{{ route('users.search') }}"
+                  method="GET"
+                  class="form form-inline">
+                <input type="text"
+                       name="filter"
+                       placeholder="Nome"
+                       class="form-control"
+                       value="{{ request()->filter }}"
+                       minlength="2">
+                <button type="submit" class="btn btn-dark">Filtrar</button>
+            </form>
+        </div>
+        <div class="card-body">
+            <table class="table-condensed table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-info">Editar</a>
+                                <a href="{{ route('users.show', $user->id) }}" class="btn btn-warning">Ver</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {!! $pagination !!}
+        </div>
+    </div>
+@stop
