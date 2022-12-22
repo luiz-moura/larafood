2ff5e965f780525462310060ea88f0832d324468
Luiz Moura <moura.oliveira.luiz@gmail.com>
2022-12-21 21:56:57 -0400

    feat(seeders): change class name to default

--- database/seeders/ClientsTableSeeder.php
+++ database/seeders/ClientsTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Infrastructure\Persistence\Eloquent\Models\Client;
 
-class ClientTableSeeder extends Seeder
+class ClientsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
